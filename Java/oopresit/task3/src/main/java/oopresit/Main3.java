e01fcbd91a1f79c6c32f9e3a6e00241c1338a693
Owen Attard <owen.attard.22@um.edu.mt>
2024-08-31 13:47:59 +0200

    First Protobuf Sucess Checkpoint

--- Java/oopresit/task3/src/main/java/oopresit/Main3.java
+++ Java/oopresit/task3/src/main/java/oopresit/Main3.java
@@ -1,9 +1,18 @@
 package oopresit;
 
+import oopresit.proto.TestProto;
+
 public class Main3 {
     public static void main(String[] args) {
         System.out.println("TASK 3!!!!!!!!!!");
-        CLI3.InitMenu();
+        // CLI3.InitMenu();
+        TestProto.SearchRequest.Builder builder = TestProto.SearchRequest.newBuilder();
+        TestProto.SearchRequest request = builder
+        .setQuery("test")
+        .setPageNumber(1)
+        .setResultPerPage(10)
+        .build();
+        System.out.println(request);
     }
 }
 
